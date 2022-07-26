1278c05fc915e85e8e06eabc39339933ea6271c5
yuutan1017 <n.you0611@gmail.com>
2022-07-26 18:23:03 +0900

    css

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,11 +2,37 @@ import React, { useState, FC } from 'react';
 import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 
 import dummyData from '../dummy';
-import { Card } from './Card';
 
 export const Main: FC = () => {
   const [data, setData] = useState(dummyData);
-  const onDragEnd = () => {};
+  const onDragEnd = (result: any) => {
+    const { source, destination } = result;
+
+    if (source.droppableId !== destination.droppableId) {
+      const sourceColIndex = data.findIndex((e) => e.id === source.droppableId);
+      const destinationColIndex = data.findIndex((e) => e.id === destination.droppableId);
+      const sourceCol = data[sourceColIndex];
+      const destinationCol = data[destinationColIndex];
+      const sourceTask = [...sourceCol.tasks];
+      const destinationTask = [...destinationCol.tasks];
+      const [removed] = sourceTask.splice(source.index, 1);
+      destinationTask.splice(destination.index, 0, removed);
+      data[sourceColIndex].tasks = sourceTask;
+      data[destinationColIndex].tasks = destinationTask;
+      setData(data);
+    } else {
+      //同じカラム内での入れ替え
+      const sourceColIndex = data.findIndex((e) => e.id === source.droppableId);
+      const sourceCol = data[sourceColIndex];
+      //破壊的プログラムなので配列をコピーする
+      const sourceTask = [...sourceCol.tasks];
+      //タスクの削除と追加
+      const [removed] = sourceTask.splice(source.index, 1);
+      sourceTask.splice(destination.index, 0, removed);
+      data[sourceColIndex].tasks = sourceTask;
+      setData(data);
+    }
+  };
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <div className="trello">
